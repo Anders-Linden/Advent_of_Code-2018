ef98262f4175923374194e611ea52f0bb28f1deb
Anders Lindén <anders@andvision.se>
2018-12-12 01:15:05 +0100

    New: Adding day 5 part 2 solution

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -24,7 +24,18 @@ fn compare_chars (formula: Vec<u8>) -> Vec<u8> {
     }
     result
 }
+fn compare_single_chars (formula: Vec<u8>, limit: u8) -> Vec<u8> {
+    let mut result: Vec<u8> = Vec::new();
+    // Array of [upper case, lower case]
+    let limit_list = [limit, limit+32u8];   
 
+    for character in formula {
+        if !limit_list.contains( &character ) {
+            result.push(character);
+        }
+    }
+    result
+}
 
 fn part1() -> usize {
     let mut reader = open_input();
@@ -40,8 +51,34 @@ fn part1() -> usize {
     data.len()-1
 }
 
+fn part2() -> usize {
+    let mut reader = open_input();
+    let mut buffer = Vec::new();
+    let mut compare = Vec::new();
+
+    match reader.read_to_end(&mut buffer) {
+        Ok(_data) => (),
+        Err(er) => println!("I/O Error: {}", er),
+    }
+
+    // ASCII Code for "A" is 65, for "a" it is A + 32 = 97
+    // "B" = 66, "b" = B+32 = 98
+    for plus in 0..26 {
+        // Going through A to Z
+        let data_a =  compare_chars(compare_single_chars(buffer.clone(), 65u8+plus));
+        compare.push(data_a.len()-1);
+    }
+
+    let value = compare.iter().min_by(|a, b| a.cmp(b));
+    
+    *value.unwrap()
+
+}
+
 fn main() {
     println!("Results for Day 5");
     println!("-------------------");
     println!("Part 1: {}",part1());
+    println!("Part 2: {}",part2());
+    part2();
 }
