be2c60db7edcf5f3af17963b222763eb11867c98
Anders Lindén <anders@andvision.se>
2019-11-01 18:14:34 +0100

    refactor: Day 1 Moved reading data out of the loop

--- src/day_01.rs
+++ src/day_01.rs
@@ -21,10 +21,12 @@ fn part2() -> i32 {
     let init_freq = 0;
     let mut result = init_freq;
     let mut resulting_freq: Vec<i32> = Vec::new();
+    let data: Vec<String> = utilities::open_input("./assets/input_day01")
+        .lines().collect::<Result<_, _>>().unwrap();
 
     loop {
-        for line in utilities::open_input("./assets/input_day01").lines() {
-            let freq_change: i32 = line.unwrap().parse().unwrap();
+        for line in &data {
+            let freq_change: i32 = line.parse().unwrap();
             result += freq_change;
 
             if resulting_freq.iter().any(|&x| x == result) {
